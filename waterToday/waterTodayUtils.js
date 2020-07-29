9e5ba28ceba58a9b134b9d25b989c79c12684291
bwapes <benwaples@gmail.com>
2020-07-29 11:53:59 -0700

    update nav bar styling and fix layout on greenhouse page

--- waterToday/waterTodayUtils.js
+++ waterToday/waterTodayUtils.js
@@ -1,11 +1,14 @@
-import plants from '../data.js';
-import { findById } from '../userUtils.js';
+import { findById, getUser } from '../userUtils.js';
+
+const user = getUser();
+
+const plants = user.plantCollection;
 
 export function renderWaterToday(plant) {
     const originPlant = findById(plants, plant.id);
 
     const li = document.createElement('li');
-    li.classList.add = 'water-today';
+    li.classList.add('water-today');
 
     const div1 = document.createElement('div');
     div1.classList.add('main-div');
@@ -16,30 +19,30 @@ export function renderWaterToday(plant) {
     div1.append(img);
     
     const iconDiv = document.createElement('div');
+    iconDiv.classList.add('icon-div');
     div1.append(iconDiv);
     
-    const i = document.createElement('i');
-    i.classList.add('fa');
-    i.classList.add('fa-sun-o');
-    i.classList.add('fa-lg');
-    i.setAttribute('aria-hidden', 'true');
-    i.style.color = 'yellow';
+    const i = document.createElement('img');
+    i.src = '../assets/sun.jpg';
+    i.classList.add('icon');
     iconDiv.append(i);
     
     const h3 = document.createElement('h3');
-    h3.textContent = plants.name;
+    h3.classList.add('h3-title');
+    h3.textContent = plant.name;
     li.appendChild(h3);
     
-    
+    const buttonDiv = document.createElement('div');
+    buttonDiv.classList.add('buttonDiv');
     const button = document.createElement('button');
     button.textContent = 'Add Water';
+    buttonDiv.append(button);
     
     button.addEventListener('click', () => {
         li.classList.add = 'checked';
         button.disabled;
     });
-    li.append(div1);
-    li.append(button);
+    li.append(div1, buttonDiv);
 
 
     return li;
