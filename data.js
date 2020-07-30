598fe69846dc9ce87cad433241282bae87c8c42a
Nathan Kruckenberg <natekruck1990@gmail.com>
2020-07-29 21:47:43 -0700

    added massive plant data to data.js

--- data.js
+++ data.js
@@ -1,22 +1,284 @@
-const sunflower = {
-    id: 234,
-    name: 'sunflower',
-    category: 'tropicals',
-    waterCare: 1,
-    sunCare: 'direct',
-    img: '../assets/sunflower.jpg'
+const calicoHearts = { 
+    id: 999, 
+    name: 'calico hearts', 
+    category: 'succulents-cactus', 
+    water: 'Minimal Watering - allow soil to dry fully', 
+    sunCare: 'indirect', 
+    img: '../assets/calico-hearts.jpg' 
 };
 
+const lemonButtonFern = {   
+    id: '200y',
+    name: 'Lemon Button Fern',
+    category: 'succulents-cactus',
+    water: 'Regular watering- Keep soil moist',
+    sunCare: 'Medium- Low indirect light',
+    img: '../assets/Lemon-Button-Fern.jpg' 
+};
 
+const anthurium = {
+    id: '211y',
+    name: 'Anthurium',
+    category: 'tropicals',
+    water: 'Moderate watering- allow top half of soil to dry',
+    sunCare: 'Medium indirect',
+    img: '../assets/Anthurium.jpg'
+};
 
+const calatheaOrbifolia = {
+    id: '322a', 
+    name: 'Calathea Orbifolia',
+    category: 'tropicals',
+    water: 'Regular watering – Keep soil fairly moist', 
+    sunCare: 'Low indirect',
+    img: '../assets/calathea.jpg'
+};
+
+const peperomiaRaindrop = { 
+    id: '333a',  
+    name: 'Peperomia Raindrop', 
+    category: 'tropicals', 
+    water: 'Moderate watering – Allow soil to dry halfway', 
+    sunCare: 'Medium indirect',
+    img: '../assets/peperomia.jpg'
+};
+
+const sweetHeartHoya = {
+    id: '201y',
+    name: 'Sweet Heart Hoya',
+    category: 'tropicals',
+    water: 'Moderate watering- allow top half of soil to dry',
+    sunCare: 'Medium indirect',
+    img: '../assets/Sweet-Heart-Hoya.jpg'
+};
+
+const sansevieriaSamurai = { 
+    id: '215y', 
+    name: 'Sansevieria Samurai', 
+    category: 'tropicals', 
+    water: 'Minimal watering- allow soil to dry', 
+    sunCare: 'Medium indirect', 
+    img: '../assets/Sanservia-Samurai.jpg' 
+};
+
+const sansevieriaZeylanica = { 
+    id: '220x', 
+    name: 'Sansevieria Zeylanica', 
+    category: 'tropicals', 
+    water: 'Minimal watering- allow soil to dry', 
+    sunCare: 'Low Indirect', 
+    img: '../assets/Sanservia-Zeylanica.jpg' };
+
+const pothosGolden = { 
+    id: '337b', 
+    name: 'Pothos Golden', 
+    category: 'tropicals', 
+    water: 'Moderate - minimal watering- allow top few inches of soil to dry', 
+    sunCare: 'Low indirect', 
+    img: '../assets/pothosgolden.jpg' };
+
+const rubberPlantTineke = { 
+    id: '223ff', 
+    name: 'Rubber Plant Tineke', 
+    category: 'tropicals', 
+    water: 'Moderate watering- Allow soil to dry part way', sunCare: 'Indirect', img: '../assets/Rubber-Plant-Ficus.jpg' };
+
+const spiderPlant = { 
+    id: '369a', 
+    name: 'Spider Plant', 
+    category: 'tropicals', 
+    water: 'Moderate watering – Allow soil to dry at least half way', 
+    sunCare: 'Low indirect', 
+    img: '../assets/spiderplant.jpg' 
+};
+
+const rubberPlantBurgundy = { 
+    id: '230we', 
+    name: 'Rubber Plant Burgundy', 
+    category: 'tropicals', 
+    water: 'Moderate watering- Allow soil to dry part way', 
+    sunCare: 'Indirect', 
+    img: '../assets/Rubber-Plant-Burgundy.jpg' 
+};
+
+const prayerPlantRed = { 
+    id: '235ill', 
+    name: 'Prayer Plant Red', 
+    category: 'tropicals', 
+    water: 'Regular watering - keep soil fairly moist', 
+    sunCare: 'Medium Indirect', 
+    img: '../assets/Prayer-Plant-Maranta.jpg' 
+};
+
+const philodendronBirkin = { 
+    id: '379c', 
+    name: 'Philodendron Birkin', 
+    category: 'tropicals', 
+    water: 'Moderate watering – Allow soil to dry at least half way', 
+    sunCare: 'Bright indirect', 
+    img: '../assets/philo-birkin.jpg' };
+
+const peperomiaRosso = { 
+    id: '236ccl', 
+    name: 'Peperomia Rosso', 
+    category: 'tropicals', 
+    water: 'Moderate watering- Allow soil to dry halfway', 
+    sunCare: 'Medium Indirect', 
+    img: '../assets/Peperomia-Rosso.jpg' 
+};
+
+const fiddleFigLeaf = { 
+    id: '399d', 
+    name: 'Fiddle Fig Leaf', 
+    category: 'tropicals', 
+    water: 'Moderate watering – Allow most of soil to dry', 
+    sunCare: 'Bright indirect', 
+    img: '../assets/fiddle-fig-leaf.jpg' 
+};
+
+const stringOfHearts = { 
+    id: '998s', 
+    name: 'string of hearts', 
+    category: 'succulents-cactus', 
+    water: 'Minimal Watering - allow soil to dry fully', 
+    sunCare: 'direct', 
+    img: '../assets/string-of-hearts.jpg' 
+};
+
+const jade = { 
+    id: '997s', 
+    name: 'jade', 
+    category: 'succulents-cactus', 
+    water: 'Minimal Watering - allow soil to dry fully', 
+    sunCare: 'direct', 
+    img: '../assets/jade.jpg' 
+};
+
+const succulentTraids = { 
+    id: '996s', 
+    name: 'succelent traids', 
+    category: 'succulents-cactus', 
+    water: 'Minimal Watering - allow soil to dry fully', 
+    sunCare: 'direct', 
+    img: '../assets/succulent-triad.jpg' 
+};
+
+const jellyBean = { 
+    id: '995s', 
+    name: 'jelly bean', 
+    category: 'succulents-cactus', 
+    water: 'Minimal Watering - allow soil to dry fully', 
+    sunCare: 'direct', 
+    img: '../assets/jelly-bean.jpg' 
+};
+
+const haworthiaFasciata = { 
+    id: '994s', 
+    name: 'haworthia fasciata', 
+    category: 'succulents-cactus', 
+    water: 'Minimal Watering - allow soil to dry fully', 
+    sunCare: 'medium indirect', 
+    img: '../assets/haworthia-fasciata.jpg' 
+};
+
+const senecioPearls = { 
+    id: '993s',  
+    name: 'senecio pearls', 
+    category: 'succulents-cactus', 
+    water: 'Minimal Watering - allow soil to dry fully', 
+    sunCare: 'direct', 
+    img: '../assets/senecio-pearls.jpg' 
+};
+       
+const senecioFishhooks = { 
+    id: '992s',
+    name: 'senecio fishhooks', 
+    category: 'succulents-cactus', 
+    water: 'Minimal Watering - allow soil to dry fully', 
+    sunCare: 'direct', 
+    img: '../assets/senecio-fishhooks.jpg' 
+};
+   
+const senecioBananas = { 
+    id: '991s', 
+    name: 'senecio bananas', 
+    category: 'succulents-cactus', 
+    water: 'Minimal Watering - allow soil to dry fully', 
+    sunCare: 'direct', 
+    img: '../assets/senecio-bananas.jpg' 
+};
+   
+const zigZagCactus = { 
+    id: '990s', 
+    name: 'zig zag cactus', 
+    category: 'succulents-cactus', 
+    water: 'Minimal Watering - allow soil to dry fully', 
+    sunCare: 'direct', 
+    img: '../assets/zig-zag-cactus.jpg' 
+};
+     
+const tillandsiaXerographica = { 
+    id: '989s', 
+    name: 'tillandsia xerographica', 
+    category: 'air-plants', 
+    water: 'Moderate Watering - soak once a week for 20 to 30 minutes', 
+    sunCare: 'indirect', 
+    img: '../assets/tillandsia.jpg' 
+};
+
+const dracaenaCompacta = { 
+    id: '240lcl', 
+    name: 'Dracaena Compacta', 
+    category: 'tropicals', 
+    water: 'Moderate watering- Allow soil to dry part way', 
+    sunCare: 'Low Indirect', 
+    img: '../assets/Dracaena-Compacta.jpg' 
+};
+
+const zzPlant = { 
+    id: '255gcl', 
+    name: 'ZZ Plant', 
+    category: 'tropicals', 
+    water: 'Minimal watering- Allow soil to fully dry', 
+    sunCare: 'Low Indirect', 
+    img: '../assets/ZZ-Plant.jpg' 
+};
+  
 export const stockPlant = {
     name: 'usersPlant',
     img: '../assets/stock-plant-img.jpg'
+
 };
 
-
 const plants = [
-    sunflower
+    calicoHearts,
+    lemonButtonFern,
+    anthurium,
+    calatheaOrbifolia,
+    sweetHeartHoya,
+    peperomiaRaindrop,
+    sansevieriaSamurai,
+    sansevieriaZeylanica,
+    pothosGolden,
+    rubberPlantTineke,
+    spiderPlant,
+    rubberPlantBurgundy,
+    prayerPlantRed,
+    philodendronBirkin,
+    peperomiaRosso,
+    fiddleFigLeaf,
+    stringOfHearts,
+    jade,
+    succulentTraids,
+    jellyBean,
+    haworthiaFasciata,
+    senecioPearls,
+    senecioFishhooks,
+    senecioBananas,
+    zigZagCactus,
+    tillandsiaXerographica,
+    dracaenaCompacta,
+    zzPlant
 ];
 
 export default plants;
