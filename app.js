a1edd6a801b3656ec27943b69aba93eea54682fa
bwapes <benwaples@gmail.com>
2020-07-31 09:30:16 -0700

    backdoor feature and  some styling

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-// import functions and grab DOM elements
 import { makeUser, setUser } from './userUtils.js';
 
 const form = document.querySelector('form');
+const returningOption = document.querySelector('#returning');
+const newOption = document.querySelector('#new');
 
-// initialize state
-
-// set event listeners to update state and DOM
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(form);
@@ -14,3 +12,22 @@ form.addEventListener('submit', (e) => {
     window.location = './plantSelection';
 });
 
+newOption.addEventListener('click', () => {
+    form.classList.remove('hidden');
+    newOption.classList.add('hidden');
+    returningOption.classList.add('hidden');
+});
+
+returningOption.addEventListener('click', () => {
+    const ifUserExists = localStorage.getItem('PlantrUser');
+
+    if (ifUserExists) {
+        window.location = './greenhouse';
+    } else {
+        alert('you don\'t have a saved profile, do your plants a favor and make one!');
+        form.classList.remove('hidden');
+        newOption.classList.add('hidden');
+        returningOption.classList.add('hidden');
+    }
+
+});
--- calendar/calendarUtils.js
+++ calendar/calendarUtils.js
@@ -1,6 +1,6 @@
 export function renderTableRow(plant) {
     const p = document.createElement('p');
-    p.textContent = plant.name;
+    p.textContent = `${plant.name} - ${plant.size}`;
 
     p.addEventListener('click', () => {
         p.style.textDecoration = 'line-through';
